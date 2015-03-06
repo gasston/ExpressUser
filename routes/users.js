f42aa9de94309d4052ffe34f969427e514d6fda0
gasston <belgacem.benltaif@gmail.com>
2015-03-06 23:57:16 +0100

    add logout and test session

--- routes/users.js
+++ routes/users.js
@@ -9,33 +9,48 @@ router.get('/', function(req, res) {
         res.json(docs);
     });
 });
-/* GET users listing. */
-router.get('/signin', function(req, res) {
-  res.render('user/signin', { title: 'Signin - Create new user' });
+
+/* GET Create new acount. */
+router.get('/signup', function(req, res) {
+  var sess = req.session;
+   if(sess.mail !== undefined && sess.mail !== "")
+       {
+         res.render('user/signup', { title: sess+'You are connected !!! ' });
+       }else
+       {
+         res.render('user/signup', { title: 'Signin - Create new user' });
+       }
 });
 
 /* Add new user . */
-router.post('/signin', function(req, res) {
+router.post('/signup', function(req, res) {
 	var db = req.db;
     var collection = db.get('usercollection');
 	/* var form */ 
 	var username = req.body.user , 
 	email = req.body.email , 
 	password = req.body.password;
-	
-    collection.insert({"username": username,"password": password,"email": email},
-    	function(err,doc){
-    		if(err)
-    		{ 
-    			res.render('user/signin',{error:"error_1"});
-    		}else
-    		{
-    			res.redirect("/");
-    		}
-    	}
-    	);
+	collection.find({email : email},{},
+                        function(e,docs){
+                            if(docs.length === 0)
+                                {  
+                                collection.insert({"username": username,"password": password,"email": email},
+    	                            function(err,doc){
+    		                            if(err)
+    		                                { 
+    			                                res.render('user/signup',{error:"error_1"});
+    		                                }else
+    		                                {
+    			                              res.redirect("/");
+    		                                }
+    	                                               }
+    	                          );
+                                }else
+                                {
+                                    res.render('user/signup',{error:'User Exist'});
+                                }
 });
-
+});
 /* GET users listing. */
 router.get('/login', function(req, res) {
   res.render('user/login', { title: 'Login - Form' });
@@ -46,18 +61,34 @@ router.post('/login', function(req, res) {
 	/* login action */ 
     var db = req.db;
     var collection = db.get('usercollection');
+    sess=req.session;
 	/* var form */ 
 	var email = req.body.email , password = req.body.password;
     collection.find({ email : email , password :password },{},function(e,docs){
                          if(docs.length > 0)
-                             {
-                                 res.json({error:"ok"});
+                             { 
+                                 sess.mail = email;
+                                 res.json({error:sess.mail});
+                      
                              }else
                              {
                                 res.json({error:"Nok"});
                              }
                           });
     
+});
+/* Logout user */ 
+/* GET users listing. */
+router.get('/logout',function(req,res){
+    req.session.destroy(function(err){
+        if(err){
+        console.log(err);
+        }
+        else
+        {
+        res.redirect('/');
+        }
+    });
 });
 
 
